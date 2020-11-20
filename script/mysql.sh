316c3733772489ff8be2abd3cd7bba434a6ff73c
Martin Hasin <63114658+matoH12@users.noreply.github.com>
2020-11-20 14:12:28 +0100

    update mysql.sh script
    change fukcionality. Fix BUG

--- script/mysql.sh
+++ script/mysql.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-myvariable=$(echo "SELECT swip FROM swzoznam" | mysql mrtgadmin -u admin -padmin)
-#myvariable= grep -e "swip" $myvariable
-
-function strip {
-    local STRING=${1#$"$2"}
-    echo ${STRING%$"$2"}
-}
+#myvariable=$(echo "SELECT swip FROM swzoznam" | mysql mrtgadmin -u admin -pyadFidth)
+myvariable=$(mysql mrtgadmin -N -uadmin -pyadFidth  -se "SELECT swip FROM swzoznam")
+echo $myvariable > /tmp/ip
+#grep -oE "\b([0-9]{1,3}\.){3}([0-9]{1,3})\b" /tmp/ip
 
 
-echo 'IP=('$(strip "$myvariable" "swip")');' > /script/IP
+echo 'IP=('$(cat /tmp/ip | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")');' > /script/IP
+
+rm -rf /tmp/ip
+
